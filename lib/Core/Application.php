cb20ca902ed36af1787baff151ac3cd0f13c04c9
taemin19 <daniel.thebault19@gmail.com>
2017-09-08 13:16:22 +0200

    Update Application.php: add config and getController method

--- lib/Core/Application.php
+++ lib/Core/Application.php
@@ -14,15 +14,37 @@ abstract class Application
 	/**
 	* @var HTTPRequest
 	* @var HTTPResponse
+	* @var Config
 	*/
 	protected $httpRequest;
 	protected $httpResponse;
+	protected $config;
 
 	public function __construct()
 	{
 		$this->name = '';
 		$this->httpRequest = new HTTPRequest($this);
 		$this->httpResponse = new HTTPResponse($this);
+		$this->config = new Config($this);
+	}
+
+	/**
+	* @return an instance of the corresponding controller
+	*/
+	public function getController()
+	{
+		$router = $this->config->getRouter();
+		
+		// Get the route matching with URL
+		$matchedRoute = $router->getRoute($this->httpRequest->requestURI());
+		
+		// Add URL variables
+		$_GET = array_merge($_GET, $matchedRoute->vars());
+
+		$controller = 'app\\'.$this->name.'\\Modules\\'.$matchedRoute->module().'\\'.$matchedRoute->module().'Controller';
+		
+    	return new $controller($this, $matchedRoute->module(), $matchedRoute->action());
+
 	}
 
 	/**
@@ -46,5 +68,10 @@ abstract class Application
 	public function httpResponse()
 	{
 		return $this->httpResponse;
+	}
+
+	public function config()
+	{
+		return $this->config;
 	}
 }
