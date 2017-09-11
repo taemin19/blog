38c5ab3cbdc28e8c30491d1dd3c1fd0a5dcf8106
taemin19 <daniel.thebault19@gmail.com>
2017-09-11 16:35:13 +0200

    Add DefaultController for home page

---
+++ app/Frontend/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\Frontend\Controller;
+
+use \Core\Controller;
+
+/**
+* Class DefaultController
+*/
+class DefaultController extends Controller
+{
+	/**
+	* Get manager and add variables to the page
+	* Home page static so no need to instantiate
+	*/
+	public function executeHome() {}
+}
