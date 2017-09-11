518277ad4c045577233ddb42ac0606b278be4fa5
taemin19 <daniel.thebault19@gmail.com>
2017-09-11 16:13:47 +0200

    Add FrontendApplication.php

---
+++ app/Frontend/FrontendApplication.php
@@ -0,0 +1,27 @@
+<?php
+namespace app\Frontend;
+
+use \Core\Application;
+
+/**
+* Class FrontendApplication
+*/
+class FrontendApplication extends Application
+{
+    
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->name = 'Frontend';
+    }
+
+    public function run()
+    {
+        $controller = $this->getController();
+        $controller->execute();
+
+        $this->httpResponse->setPage($controller->page());
+        $this->httpResponse->send();
+    }
+}
