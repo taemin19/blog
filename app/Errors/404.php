e3b424b02401f1c39314dd3ffe25b713eae9ea84
taemin19 <daniel.thebault19@gmail.com>
2017-09-12 11:20:51 +0200

    Add 404 page view: HTML

---
+++ app/Errors/404.php
@@ -0,0 +1,28 @@
+{% extends 'layout.php' %}
+
+{% block head %}
+<title>Page Not Found</title>
+{% endblock %}
+
+{% block navbar %}
+navbar-inverse
+{% endblock %}
+
+{% block content %}
+
+<header>
+	<div class="container">
+		<div class="row">
+
+		 	<div class="header-content">
+        		<div class="header-content-inner">
+					<h1>Page Not Found</h1>
+    				<p>Sorry, but the page you were trying to view does not exist.</p>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</header>
+
+{% endblock %}
