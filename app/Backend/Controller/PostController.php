29be7f209a073256540757e1f9aa7e58c3768d24
taemin19 <daniel.thebault19@gmail.com>
2017-09-26 10:06:53 +0200

    Add PostController class with methods to add post

---
+++ app/Backend/Controller/PostController.php
@@ -0,0 +1,98 @@
+<?php
+namespace app\Backend\Controller;
+
+use \Core\Controller;
+use \Core\HTTPRequest;
+use \Core\FormValidator;
+use \Core\Entity\Post;
+
+
+/**
+* Class PostController
+*/
+class PostController extends Controller
+{
+	/**
+	* Get a list of post and add a post
+    * @param HTTPRequest $request
+	* @return void
+	*/
+	public function executePostInsert(HTTPRequest $request)
+	{
+		// Get post manager
+    	$manager = $this->getManager();
+    	$listPost = $manager->getList();
+        
+        // Get characters limit of post content 
+        $postCharacters = $this->app->config()->get('post_characters');	
+        foreach ($listPost as $post) 
+        {
+        	if (strlen($post->content()) > $postCharacters) 
+        	{
+        		$abstract = substr($post->content(), 0, $postCharacters);
+        		$abstract = substr($abstract, 0, strrpos($abstract, ' ')). '...';
+        		$post->setContent($abstract);
+        	}
+        }
+
+        // Add post list to the page
+    	$this->page->addVar('listPost', $listPost);
+
+        // Add post in the database
+    	if ($request->requestMethod() == 'POST' ) 
+		{
+			$this->processForm($request);
+				
+			$this->app->httpResponse()->redirect('/admin/post-insert/#add-form');
+		}
+
+	}
+
+    /**
+    * Check if form is valid and save post in database
+    * @param HTTPRequest $request
+    */
+    public function processForm(HTTPRequest $request)
+    {
+        // Fields validation with rules
+        $validator = new FormValidator($_POST);
+        $validator->check('name', 'required');
+        $validator->check('name', 'maxLength', 40);
+        $validator->check('email', 'required');
+        $validator->check('email', 'email');
+        $validator->check('subject', 'required');
+        $validator->check('subject', 'maxLength', 100);
+        $validator->check('message', 'required');
+
+        // Form validation
+        if ($validator->isValid()) 
+        {
+            $post = new Post([
+                'author' => $request->postData('author'),
+                'title' => $request->postData('title'),
+                'lead' => $request->postData('lead'),
+                'content' => $request->postData('content')
+            ]);
+
+            // Check if the post is new (add) or not (update)
+            if ($request->getExists('id')) 
+            {
+                $post->setId($request->getData('id'));
+            }
+
+            $manager = $this->getManager();
+            $manager->save($post);
+
+            // User message
+            $this->app->user()->setFlash('success', $post->isNew() ? 'Your post has been successfully add.' : 'The post has been successfully update.');
+        }
+        else
+        {
+            // Pre-fill the form with valid fiels
+            $this->app->user()->setFlash('validFields', $validator->validFields());
+            // User message
+            $this->app->user()->setFlash('errors', $validator->errors());
+        }
+    }
+
+}
