e9ec4dd508664396409bac0a11d03b8f08515937
taemin19 <daniel.thebault19@gmail.com>
2017-09-26 10:23:25 +0200

    Update Twig template directory

--- lib/Core/TwigFactory.php
+++ lib/Core/TwigFactory.php
@@ -13,10 +13,11 @@ class TwigFactory
 	public static function getTwigEnvironment($app)
   	{
       require_once __DIR__.'/../vendor/autoload.php';
-    	$templateDir = __DIR__.'/../../app/'.$app.'/Templates';
+    	$templateDir = __DIR__.'/../../app/Frontend/Templates';
+      $templateDir2 = __DIR__.'/../../app/Backend/Templates';
     	$viewDir = __DIR__.'/../../app/'.$app.'/Views';
     	$errorDir = __DIR__.'/../../app/Errors';
-    	$loader = new \Twig_Loader_Filesystem(array($templateDir, $viewDir, $errorDir));
+    	$loader = new \Twig_Loader_Filesystem(array($templateDir, $templateDir2, $viewDir, $errorDir));
     	$twig = new \Twig_Environment($loader, [
         'cache' => false, //__DIR__.'/../../app/tmp'
       ]);
